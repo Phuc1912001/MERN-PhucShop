2ea972ae43397fda0c8037fe2f31b1d515ed75cd
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-20 15:50:03 +0700

    sendemail & forgot password

--- Backend/controller/userCtrl.js
+++ Backend/controller/userCtrl.js
@@ -5,6 +5,8 @@ const { generateToken } = require("../config/jwtToken");
 const validateMongoDbId = require("../utils/validateMongodbId");
 const { generateRefreshToken } = require("../config/refreshtoken");
 const jwt = require("jsonwebtoken");
+const sendEmail = require("./emailCtrl");
+const nodemailer = require("nodemailer");
 
 // register user
 const createUser = asyncHandler(async (req, res) => {
@@ -189,6 +191,80 @@ const unblockUser = asyncHandler(async (req, res) => {
   }
 });
 
+const updatePassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { password } = req.body;
+  validateMongoDbId(_id);
+  const user = await User.findById(_id);
+  if (password) {
+    user.password = password;
+    const updatedPassword = await user.save();
+    res.json(updatedPassword);
+  } else {
+    res.json(user);
+  }
+});
+
+// const forgotPasswordToken = asyncHandler(async (req, res) => {
+//   const { email } = req.body;
+//   const user = await User.findOne({ email });
+//   if (!user) throw new Error("User not found with this email");
+//   try {
+//     const token = await user.createPasswordResetToken();
+//     await user.save();
+//     let mailTransporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: "phucphuc1912001@gmail.com",
+//         pass: "aqrsuxldhvfumcbr",
+//       },
+//     });
+
+//     let details = {
+//       from: "phucphuc19120012gmail.com",
+//       to: email,
+//       subject: "Forgot Password Link : Link lấy lại mật khẩu",
+//       text: "Chào bạn nhé ",
+//       html: `Hi, Please follow this link to reset Your Password.
+//        This link is valid till 10 minutes from now. 
+//        <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</>`,
+//     };
+
+//     mailTransporter.sendMail(details, (err) => {
+//       if (err) {
+//         console.log("lỗi rồi ", err);
+//       } else {
+//         console.log("thành công rồi ");
+//       }
+//     });
+
+//     res.json(token);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// });
+
+const forgotPasswordToken = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User not found with this email");
+  try {
+    const token = await user.createPasswordResetToken();
+    await user.save();
+    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</>`;
+    const data = {
+      to: email,
+      text: "Hey User",
+      subject: "Forgot Password Link",
+      htm: resetURL,
+    };
+    sendEmail(data);
+    res.json(token);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createUser,
   loginUserCtrl,
@@ -200,4 +276,6 @@ module.exports = {
   unblockUser,
   handleRefreshToken,
   logout,
+  updatePassword,
+  forgotPasswordToken,
 };
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
-
+const crypto = require("crypto");
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema(
   {
@@ -38,9 +38,7 @@ var userSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
-    address: {
-      type: String,
-    },
+    address: [{ type: mongoose.Schema.ObjectId, ref: "Address" }],
     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
     refreshToken: {
       type: String,
@@ -55,12 +53,25 @@ var userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  const salt = bcrypt.genSaltSync(10);
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
-// nếu mk khớp nhau thì trả về true , nếu k thì trả về false
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+userSchema.methods.createPasswordResetToken = async function () {
+  const resettoken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resettoken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; // 10 minutes
+  return resettoken;
+};
+
 //Export the model
 module.exports = mongoose.model("User", userSchema);
