72d0420335210bf3a1eef14bd2672b84a41cdb54
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-20 20:27:27 +0700

    blog CRUD , like & dislike

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 // tuyến đường
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
 
 app.use(notFound);
 app.use(errorHandler);
