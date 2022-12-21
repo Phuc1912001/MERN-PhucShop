a6a5360fe1cf99b8d66b1169e92e36c7578585cd
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-21 12:49:47 +0700

    category

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
+
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 
@@ -23,6 +25,7 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
