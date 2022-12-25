e4d4b6e80c1e98eea9cca980eeb1c38d03669bed
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-25 16:43:49 +0700

    header & footer done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center ">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center ">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb=0 text-white ">Sign Up for News Letter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your Email Address..."
+                  aria-label="Your Email Address..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Address: Thái Phù - Mai Đình - Sóc Sơn - Hà Nội <br />
+                  PinCode: 10000
+                </address>
+                <a href="tel:+84 329609726" className="mt-4 d-block mb-3 ">
+                  +84 329609726
+                </a>
+                <a
+                  href="mailto:phucphuc1912001@gmail.com"
+                  className="mt-4 d-block mb-3 "
+                >
+                  phucphuc1912001@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 ">
+                  <a
+                    href="https://github.com/Phuc1912001?tab=repositories"
+                    className="text-white fs-4"
+                  >
+                    <BsLinkedin />
+                  </a>
+                  <a
+                    href="https://github.com/Phuc1912001?tab=repositories"
+                    className="text-white fs-4"
+                  >
+                    <BsInstagram />
+                  </a>
+                  <a
+                    href="https://github.com/Phuc1912001?tab=repositories"
+                    className="text-white fs-4"
+                  >
+                    <BsGithub />
+                  </a>
+                  <a
+                    href="https://github.com/Phuc1912001?tab=repositories"
+                    className="text-white fs-4"
+                  >
+                    <BsYoutube />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column ">
+                <Link className="text-white py-2 mb-1 ">Privacy</Link>
+                <Link className="text-white py-2 mb-1 ">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1 ">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1 ">
+                  Terms & Conditions
+                </Link>
+                <Link className="text-white py-2 mb-1 ">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column ">
+                <Link className="text-white py-2 mb-1 ">About Us</Link>
+                <Link className="text-white py-2 mb-1 ">Faq</Link>
+                <Link className="text-white py-2 mb-1 ">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column ">
+                <Link className="text-white py-2 mb-1 ">Laptops</Link>
+                <Link className="text-white py-2 mb-1 ">Headphones</Link>
+                <Link className="text-white py-2 mb-1 ">Tablet</Link>
+                <Link className="text-white py-2 mb-1 ">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="col-12">
+            <div className="text-center mb-0 text-white ">
+              @copy:{new Date().getFullYear()}: Powered By Nguyễn Văn Phúc{" "}
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
