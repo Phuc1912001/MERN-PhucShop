88d51ef93ff7893273472bf096d9a665011e8e3b
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-26 20:59:48 +0700

    ourStore

--- frontend/src/pages/OurStore.jsx
+++ frontend/src/pages/OurStore.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactStars from "react-rating-stars-component";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 
@@ -13,9 +14,233 @@ const OurStore = () => {
             <div className="col-3">
               <div className="filter-card mb-3 ">
                 <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3 ">
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availability</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label htmlFor="" className="form-check-label">
+                        In Stock(1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label htmlFor="" className="form-check-label">
+                        Our of Stock(0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10 ">
+                    <div className="form-floating  ">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="From"
+                      />
+                      <label htmlFor="floatingInput"> From </label>
+                    </div>
+                    <div className="form-floating  ">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="To"
+                      />
+                      <label htmlFor="floatingInput"> To </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Color</h5>
+                  <div>
+                    <ul className="colors ps-0 ">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-1"
+                      />
+                      <label htmlFor="color-1" className="form-check-label">
+                        S (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-2"
+                      />
+                      <label htmlFor="color-2" className="form-check-label">
+                        M (2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3 ">
+                <h3 className="filter-title">Product Tags</h3>
+                <div className=" product-tags d-flex flex-wrap align-items-center gap-10 ">
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3 ">
+                    Headphone
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3 ">
+                    Laptop
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3 ">
+                    Mobile
+                  </span>
+                  <span className="badge bg-light text-secondary rounded-3 py-2 px-3 ">
+                    Wire
+                  </span>
+                </div>
+              </div>
+              <div className="filter-card mb-3 ">
+                <h3 className="filter-title">Random Products</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphone bulk 10 pack multi color for student
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphone bulk 10 pack multi color for student
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid">
+                <div className="d-flex justify-content-between align-items-center ">
+                  <div className="d-flex align-items-center gap-10 ">
+                    <p className="mb-0 d-block " style={{ width: "100px" }}>
+                      Sort By:
+                    </p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual"> Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically,Z-A
+                      </option>
+                      <option value="title-ascending">
+                        Price, Low to High
+                      </option>
+                      <option value="title-descending">
+                        Price,High to Low
+                      </option>
+                      <option value="title-ascending">Date, Old to New</option>
+                      <option value="title-descending">
+                        Date , New to Old
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10 ">
+                    <p className="totalproducts mb-0">21 Product</p>
+                    <div className="d-flex gap-10  align-items-center grid ">
+                      <img
+                        src="images/gr4.svg"
+                        className="d-block img-fluid "
+                        alt="grid"
+                      />
+                      <img
+                        src="images/gr3.svg"
+                        className="d-block img-fluid "
+                        alt="grid"
+                      />
+                      <img
+                        src="images/gr2.svg"
+                        className="d-block img-fluid "
+                        alt="grid"
+                      />
+                      <img
+                        src="images/gr.svg"
+                        className="d-block img-fluid "
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
